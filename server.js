183c6a7261fc0ba89780ec81d64fa17705c66a1b
candrasaga <candra.setiawan9b@gmail.com>
2024-06-09 15:02:24 +0700

    Updated server.js with cors and fixed other dependencies

--- server.js
+++ server.js
@@ -1,4 +1,17 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
-app.unsubscribe(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('home');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
