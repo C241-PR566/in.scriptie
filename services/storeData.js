693bb3842887b16393ae4fb866a43bbf82ec5dd7
candra <103479361+candrasaga@users.noreply.github.com>
2024-06-19 15:40:43 +0700

    added some database connection logs

--- services/storeData.js
+++ services/storeData.js
@@ -1,5 +1,5 @@
-import dotenv from 'dotenv';
 import mysql from "mysql";
+import dotenv from "dotenv";
 
 dotenv.config();
 
@@ -7,7 +7,15 @@ const conn = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+});
+
+conn.connect((err) => {
+  if (err) {
+    console.error('Error connecting to the database: ' + err.stack);
+    return;
+  }
+  console.log('Connected to the database as id ' + conn.threadId);
 });
 
 export default conn;
